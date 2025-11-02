097447878002c3da4a9a2dbeace7de990c8e7198
dutixlf <dutixlf@gmail.com>
2025-11-02 19:45:15 +0500

    feat(app/window): added window close/minimize/restore/minimize functions

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 type MusicApp struct {
@@ -14,4 +16,20 @@ func NewApp() *MusicApp {
 
 func (a *MusicApp) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *MusicApp) Minimize(ctx context.Context) {
+	runtime.WindowMinimise(ctx)
+}
+
+func (a *MusicApp) Maximize(ctx context.Context) {
+	runtime.WindowMaximise(ctx)
+}
+
+func (a *MusicApp) Restore(ctx context.Context) {
+	runtime.WindowUnmaximise(ctx)
+}
+
+func (a *MusicApp) Close(ctx context.Context) {
+	runtime.Quit(ctx)
 }
