18c28c7fec45bd570f1fc18bba5961c22356021f
notlikethiss <notlikeethiss@gmail.com>
2025-12-07 15:39:54 +0300

    fix: icon keywords

--- frontend/app/components/icons/back.tsx
+++ frontend/app/components/icons/back.tsx
@@ -12,7 +12,7 @@ const BackIcon: FC<IconProps> = ({ width = 16, height = 16, className, ...props 
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g opacity="0.75" clip-path="url(#clip0_4_26)">
+      <g opacity="0.75" clipPath="url(#clip0_4_26)">
         <rect width="4" height="16" rx="2" fill="white" />
         <path
           d="M14.4453 1.03647L5.24808 7.16795C4.65434 7.56377 4.65434 8.43623 5.24807 8.83205L14.4453 14.9635C15.1099 15.4066 16 14.9302 16 14.1315V1.86852C16 1.06982 15.1099 0.59343 14.4453 1.03647Z"
--- frontend/app/components/icons/track-info.tsx
+++ frontend/app/components/icons/track-info.tsx
@@ -12,7 +12,7 @@ const TrackInfoIcon: FC<IconProps> = ({ width = 14, height = 16, className, ...p
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect x="1" y="1" width="12" height="14" rx="1" stroke="#8FCFEF" stroke-width="2" />
+      <rect x="1" y="1" width="12" height="14" rx="1" stroke="#8FCFEF" strokeWidth="2" />
       <path d="M5 11V5L10 8L5 11Z" fill="#8FCFEF" />
     </svg>
   );
